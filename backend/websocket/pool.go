eae6d135d6f20cd6fd487feefec1ba9820dbb8da
rakshith <rakshit.s.gowda@gmail.com>
2023-07-20 09:52:08 +0530

    backend added

---
+++ backend/websocket/pool.go
@@ -0,0 +1,51 @@
+package customwebsocket
+
+import "fmt"
+
+type Pool struct {
+	Register   chan *Client
+	UnRegister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		UnRegister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message)}
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("total connection pool:- ", len(pool.Clients))
+			for k, _ := range pool.Clients {
+				fmt.Println(k)
+				k.Conn.WriteJSON(Message{Type:1,Body:"New User Joined"})
+			}
+		
+
+		case client := <-pool.UnRegister:
+			delete(pool.Clients, client)
+			fmt.Println("total connection pool:- ",len(pool.Clients))
+			for k , _ := range pool.Clients{
+				fmt.Println(k)
+				k.Conn.WriteJSON(Message{Type:1,Body:"User Deleted"})
+			}
+			
+
+		case msg := <-pool.Broadcast:
+			fmt.Println("BroadCasting a message")
+			for k,_:=range pool.Clients{
+				if err:=k.Conn.WriteJSON(msg);err!=nil{
+						fmt.Println(err)
+						return
+				}
+			}
+		}
+	}
+}
